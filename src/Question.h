7228a2ee12ddd8528d78f49c694c4fb36052a658
Clang Robot <robot@example.com>
2021-12-04 21:17:37 +0000

    Committing clang-format changes

--- src/Question.h
+++ src/Question.h
@@ -3,117 +3,88 @@
 #include <iostream>
 
 namespace MathsQuiz {
-    
-    class Question
-    {
-    public:
-        enum class QuestionType : int
-        {
-            LongMultiplication = 1,
-            ShortDivison = 2
-        };
 
-    private:
-        QuestionType m_QuestionType;
+class Question {
+public:
+  enum class QuestionType : int { LongMultiplication = 1, ShortDivison = 2 };
 
-        int m_LongMultiplication[2];
-        int m_ShortDivision[2];
+private:
+  QuestionType m_QuestionType;
 
-        int m_LongMultiplicationAnswer;
-        int m_ShortDivisionAnswer[2];
+  int m_LongMultiplication[2];
+  int m_ShortDivision[2];
 
-        int m_LongMultiplicationUserAnswer;
-        int m_ShortDivisionUserAnswer[2];
+  int m_LongMultiplicationAnswer;
+  int m_ShortDivisionAnswer[2];
 
-        int m_LongMultiplicationCorrect;
-        int m_ShortDivisionCorrect;
+  int m_LongMultiplicationUserAnswer;
+  int m_ShortDivisionUserAnswer[2];
 
-    public:
-        void SetType(QuestionType questionType)
-        {
-            m_QuestionType = questionType;
-        }
+  int m_LongMultiplicationCorrect;
+  int m_ShortDivisionCorrect;
 
-        std::string NextQuestion()
-        {
-            if (m_QuestionType == QuestionType::LongMultiplication)
-            {
-                return NextLongMultiplication();
-            }
-            else
-            {
-                return NextShortDivision();
-            }
-        }
+public:
+  void SetType(QuestionType questionType) { m_QuestionType = questionType; }
 
-        void UserAnswer()
-        {
-            if (m_QuestionType == QuestionType::LongMultiplication)
-            {
-                LongMultiplicationUserAnswer();
-            }
-            else
-            {
-                ShortDivisionAnswerUserAnswer();
-            }
-        }
+  std::string NextQuestion() {
+    if (m_QuestionType == QuestionType::LongMultiplication) {
+      return NextLongMultiplication();
+    } else {
+      return NextShortDivision();
+    }
+  }
 
-        std::string Answer()
-        {
-            if (m_QuestionType == QuestionType::LongMultiplication)
-            {
-                return LongMultiplicationAnswer();
-            }
-            else
-            {
-                return ShortDivisionAnswer();
-            }
-        }
+  void UserAnswer() {
+    if (m_QuestionType == QuestionType::LongMultiplication) {
+      LongMultiplicationUserAnswer();
+    } else {
+      ShortDivisionAnswerUserAnswer();
+    }
+  }
 
-        int Correct()
-        {
-            if (m_QuestionType == QuestionType::LongMultiplication)
-            {
-                return m_LongMultiplicationCorrect;
-            }
-            else
-            {
-                return m_ShortDivisionCorrect;
-            }
-        }
+  std::string Answer() {
+    if (m_QuestionType == QuestionType::LongMultiplication) {
+      return LongMultiplicationAnswer();
+    } else {
+      return ShortDivisionAnswer();
+    }
+  }
 
-        std::string Solution()
-        {
-            if (m_QuestionType == QuestionType::LongMultiplication)
-            {
-                return LongMultiplicationSolution();
-            }
-            else
-            {
-                return ShortDivisionSolution();
-            }
-        }
+  int Correct() {
+    if (m_QuestionType == QuestionType::LongMultiplication) {
+      return m_LongMultiplicationCorrect;
+    } else {
+      return m_ShortDivisionCorrect;
+    }
+  }
 
-    private:
-        std::string NextLongMultiplication();
+  std::string Solution() {
+    if (m_QuestionType == QuestionType::LongMultiplication) {
+      return LongMultiplicationSolution();
+    } else {
+      return ShortDivisionSolution();
+    }
+  }
 
-        std::string NextShortDivision();
+private:
+  std::string NextLongMultiplication();
 
-        std::string LongMultiplicationAnswer();
+  std::string NextShortDivision();
 
-        std::string ShortDivisionAnswer();
+  std::string LongMultiplicationAnswer();
 
-        std::string LongMultiplicationSolution();
+  std::string ShortDivisionAnswer();
 
-        std::string ShortDivisionSolution();
+  std::string LongMultiplicationSolution();
 
-        void LongMultiplicationUserAnswer();
+  std::string ShortDivisionSolution();
 
-        void ShortDivisionAnswerUserAnswer();
+  void LongMultiplicationUserAnswer();
 
-        int RandomNumber(int min, int max)
-        {
-            return min + (rand() % static_cast<int>(max - min + 1));
-        }
-    };
-}
+  void ShortDivisionAnswerUserAnswer();
+
+  int RandomNumber(int min, int max) {
+    return min + (rand() % static_cast<int>(max - min + 1));
+  }
+};
+} // namespace MathsQuiz
