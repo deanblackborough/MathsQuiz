5e63a279f6770074a350c42d0ad1485be42b803b
Dean Blackborough <dean@g3d-development.com>
2021-12-05 15:13:33 +0000

    Premake
    - Use premake to generate the project

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+workspace "MathsQuiz"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MathsQuiz"
+    location "MathsQuiz"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin/intermediates/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/resource/**.ico",
+    }
+
+    filter "system.windows"
+        cppdialect "C++20"
+        systemversion "latest"
